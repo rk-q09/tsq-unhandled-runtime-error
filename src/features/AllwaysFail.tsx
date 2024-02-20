5ebfb3b9d862f197ae6675eb8cec628b60369458
riki horie <rikihorie@rikinoMacBook-Pro.local>
2024-02-20 19:33:09 +0900

    AllwaysFailをssr: falsenに

--- src/features/AllwaysFail.tsx
+++ src/features/AllwaysFail.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { useQuery } from "@tanstack/react-query";
 
 function failingQuery() {
   throw new Error("oh no");
 }
-export const AllwaysFail = () => {
+
+const AllwaysFailComponent = () => {
   const { data } = useQuery({
     queryKey: ["always fail"],
     queryFn: failingQuery,
@@ -15,3 +17,10 @@ export const AllwaysFail = () => {
 
   return <div>allways fail</div>;
 };
+
+export const AllwaysFail = dynamic(
+  () => Promise.resolve(AllwaysFailComponent),
+  {
+    ssr: false,
+  },
+);
